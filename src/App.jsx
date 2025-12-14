b05631cc81944fcf3bffc2cd10f99775c15671b5
Mriganaav <mriganaav.kashyap@gmail.com>
2025-12-14 12:23:58 +0530

    fixed caching

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "./components/hero/Hero";
+import Hero from "./components/hero/Hero"
 import Projects from "./components/projects/Projects";
 import Certificates from "./components/certificates/Certificates";
 import Contact from "./components/contact/Contact";
